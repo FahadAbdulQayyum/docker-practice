f2540d159e2288c6064de6781b2e9eda372d886d
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2024-12-20 11:14:41 +0500

    express package installed and used in index.ts to create API

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
-console.log("Hello There...");
-for (let i = 0; i < 10; i++ ) {
-    console.log("You're counting:", i);
-}
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
+  res.send("Hello There...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
