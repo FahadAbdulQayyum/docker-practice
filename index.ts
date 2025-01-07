82c58e591a048d7d19990f72bf6929067c7fba74
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-01-07 22:33:01 +0500

    Request & Response typing

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-// app.get("/", (req: Request, res: Response) => {
-app.get("/", (req, res) => {
-  res.json({msg: "Hello There..."});
+app.get("/", (req: Request, res: Response) => {
+// app.get("/", (req, res) => {
+  res.json({ msg: "Hello There..." });
 });
 
 app.listen(PORT, () => {
