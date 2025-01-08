4f8dff74f019daf35de4b8bb462519e391ae2df4
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-01-09 00:22:01 +0500

    /created/:id api created built

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,16 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 app.get("/", (req, res) => {
-    res.json({ msg: "Hello There..." });
+    res.json({ msg: "Hello There...!" });
+});
+app.get("/about", (req, res) => {
+    res.json({ msg: "About Page...!" });
+});
+app.get("/contact", (req, res) => {
+    res.json({ msg: "Contact Page...!" });
+});
+app.get("/contact/:id", (req, res) => {
+    res.json({ msg: "Contact Page...!", id: req.params.id });
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
